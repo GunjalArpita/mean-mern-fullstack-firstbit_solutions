3a771d3c21d662aa34d126ea99d190107c4dcc37
GunjalArpita <gunjalarpita80@gmail.com>
2026-06-22 19:01:34 +0530

    problem solved

--- javascript/questions.js
+++ javascript/questions.js
@@ -13,8 +13,8 @@
 // case 3 : if given num is bigger than all array element, then print num is out of range.
 
 
-const array = [5,20,50,100,64,90];
-//case 1
+ const array = [5,20,50,100,64,90];
+// //case 1
 let num = 575;
 let index = array.indexOf(num);
 
@@ -31,7 +31,7 @@ if (nextBigger !== undefined) {
     console.log(`${num} is out of range`);
 }
 
-//case 3
+// //case 3
 let isOutOfRange = array.every((element) => element < num);
 if (isOutOfRange) {
     console.log(`${num} is out of range`);
@@ -42,11 +42,59 @@ if (isOutOfRange) {
 //const str='arpita gunjal'
 //output='a1p2t3 g4n5a6
 
+// const str = 'arpita gunjal';
+// let res ='';
+//  let c = 1
+// for(let i=0;i<str.length;i++)
+
+// {
+   
+//     if(i%2==0)
+//     {
+//         res = str[i].replace(c+2);
+       
+//     }
+//     // else if(i===' ')
+//     // {
+
+//     // }
+// }
+// console.log(res);
+
+
 //Question-5
 //length of array without inbuild function
 
+
+const arrays = [1,2,3,4,5,6,7,8,9,10,11];
+console.log(arrays);
+
+let count = 0
+for(let i in arrays)
+{
+     count+=1;
+}
+console.log("length is " + count);
+
+
 //Question -6
 //sort
+
+const nums = [54,45,23,78,2436,67898];
+for(let i=0;i<nums.length;i++)
+{
+    for(let j=0;j<nums.length;j++)
+    {
+        if(nums[j]>nums[j+1])
+            {
+            let temp = nums[j];
+            nums[j]=nums[j+1];
+            nums[j+1]=temp;
+        }
+    }
+}
+
+console.log(nums);
 
 
 
